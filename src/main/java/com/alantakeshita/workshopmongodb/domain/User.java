7c61e51a09c5bdf00b29ae0bb839343097a91d4d
Alan Takeshita <ftmast@gmail.com>
2019-02-18 10:20:20 -0300

    Obtendo usuario por id

--- src/main/java/com/alantakeshita/workshopmongodb/domain/User.java
+++ src/main/java/com/alantakeshita/workshopmongodb/domain/User.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@org.springframework.data.annotation.Id
-	private String Id;
+	private String id;
 	private String name;
 	private String email;
 	
@@ -18,17 +18,17 @@ public class User implements Serializable{
 
 	public User(String id, String name, String email) {
 		super();
-		Id = id;
+		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
 	public String getId() {
-		return Id;
+		return this.id;
 	}
 
 	public void setId(String id) {
-		Id = id;
+		this.id = id;
 	}
 
 	public String getName() {
@@ -51,7 +51,7 @@ public class User implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -64,10 +64,10 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (Id == null) {
-			if (other.Id != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!Id.equals(other.Id))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
--- src/main/java/com/alantakeshita/workshopmongodb/services/UserService.java
+++ src/main/java/com/alantakeshita/workshopmongodb/services/UserService.java
@@ -1,20 +1,32 @@
 package com.alantakeshita.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alantakeshita.workshopmongodb.domain.User;
 import com.alantakeshita.workshopmongodb.repository.UserRepository;
+import com.alantakeshita.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new
+	 ObjectNotFoundException("Objeto não encontrado"));
+	 
+	 }
+	 
+
 }
