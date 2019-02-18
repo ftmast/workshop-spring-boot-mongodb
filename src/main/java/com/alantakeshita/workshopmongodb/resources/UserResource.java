9859bf8e3deb971a037263c630fbc3aa1e370d75
Alan Takeshita <ftmast@gmail.com>
2019-02-18 11:01:46 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/alantakeshita/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/alantakeshita/workshopmongodb/resources/UserResource.java
@@ -55,4 +55,10 @@ public class UserResource {
 		
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
