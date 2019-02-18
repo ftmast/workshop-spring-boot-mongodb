2ea777697a608527cde3f80efb3dd56c5abb94a5
Alan Takeshita <ftmast@gmail.com>
2019-02-18 07:56:05 -0300

    Entity user e REST funcionando

---
+++ src/main/java/com/alantakeshita/workshopmongodb/domain/User.java
@@ -0,0 +1,71 @@
+package com.alantakeshita.workshopmongodb.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private String Id;
+	private String name;
+	private String email;
+	
+	public User() {
+		
+	}
+
+	public User(String id, String name, String email) {
+		super();
+		Id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+}
