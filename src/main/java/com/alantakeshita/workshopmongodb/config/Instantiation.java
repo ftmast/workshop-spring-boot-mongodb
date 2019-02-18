a0e55d4b6229383f6ed2d38b5b2e5a8dae8188e8
Alan Takeshita <ftmast@gmail.com>
2019-02-18 08:49:51 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/alantakeshita/workshopmongodb/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.alantakeshita.workshopmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.alantakeshita.workshopmongodb.domain.User;
+import com.alantakeshita.workshopmongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRep;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRep.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User alan = new User(null, "Alan Takeshita", "alan@gmail.com");
+		
+		userRep.saveAll(Arrays.asList(maria, alex, bob, alan));
+	}
+
+}
